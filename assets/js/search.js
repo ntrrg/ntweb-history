61ec34ce9c73e3242e28427e9aeb870754c6b11b
Miguel Angel Rivera Notararigo <ntrrgx@gmail.com>
2020-02-07 17:09:10 -0400

    Add a loader in the search form

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,14 +1,59 @@
-function Base64Encode(str) {
+function base64Encode(str) {
   const encoder = new TextEncoder()
   return base64js.fromByteArray(encoder.encode(str))
 }
 
-function Base64Decode(str) {
+function base64Decode(str) {
   const decoder = new TextDecoder()
   return decoder.decode(base64js.toByteArray(str));
 }
 
+function startLoader(interval = 100) {
+  const el = document.querySelector('#search-box .loader')
+
+  if (typeof el.dataset.interval !== 'undefined') {
+    return
+  }
+
+  let count = 0
+
+  el.dataset.interval = setInterval(() => {
+    switch (count % 4) {
+      case 0:
+        el.innerText = '|'
+        break
+      case 1:
+        el.innerText = '/'
+        break
+      case 2:
+        el.innerText = '-'
+        break
+      case 3:
+        el.innerText = '\\'
+        break
+    }
+
+    count++
+  }, interval)
+}
+
+function stopLoader() {
+  const el = document.querySelector('#search-box .loader')
+
+  if (typeof el.dataset.interval === 'undefined') {
+    return
+  }
+
+  clearInterval(el.dataset.interval)
+  el.innerText = ''
+  el.removeAttribute('data-interval')
+}
+
 async function buildSearchIndex() {
+  const input = document.querySelector('#search-box input')
+  input.setAttribute('disabled', '')
+  startLoader()
+
   const res = await fetch('../search-index/index.json')
   const data = await res.json()
   window.idxData = new Map()
@@ -21,15 +66,18 @@ async function buildSearchIndex() {
     this.field('content')
 
     data.forEach(function (doc) {
-      doc.content = Base64Decode(doc.content)
+      doc.content = base64Decode(doc.content)
       this.add(doc)
       window.idxData.set(doc.url, doc)
     }, this)
+
+    input.removeAttribute('disabled')
+    stopLoader()
   })
 }
 
 async function search(q) {
-  window.searching = true
+  startLoader()
 
   if (window.idx === undefined) {
     await buildSearchIndex()
@@ -56,7 +104,7 @@ async function search(q) {
     resultsEl.innerHTML += html
   }
 
-  window.searching = false
+  stopLoader()
 }
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -72,7 +120,7 @@ window.addEventListener('DOMContentLoaded', () => {
 document.querySelector('#search-box').addEventListener('submit', (e) => {
   const q = e.target.querySelector('input').value
 
-  if (!window.searching && q) {
+  if (q) {
     search(q)
   }
 
